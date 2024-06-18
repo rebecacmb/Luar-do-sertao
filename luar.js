51f606f27dc7e8ffaa54d9d962eb0e9c982f69f1
rebecacmb <129316755+rebecacmb@users.noreply.github.com>
2024-06-18 08:10:04 -0300

    sdfsfg

---
+++ luar.js
@@ -0,0 +1,17 @@
+const botoes= document.querySelectorAll('.objetivos');
+// botoes.length
+const texto= document.querySelectorAll('.aba-conteudo');
+
+for (let i = 0; i < botoes.length; i++) {
+    console.log(i);
+    botoes[i].onclick = function(){
+    for(let j = 0; j < botoes.length; j++){
+        botoes[j].classList.remove('ativo');
+        texto[j].classList.remove('ativa');
+    }
+    botoes[i].classList.add('ativo'); 
+    texto[i].classList.add('ativa'); }
+    
+    //botoes[i].classList.remove('ativo');
+
+}
